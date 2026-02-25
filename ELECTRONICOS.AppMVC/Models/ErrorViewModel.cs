3acc70086634bdc73f938750f4b55e9cf134f7c5
MINEDUCYT <MINEDUCYT@REINA>
2026-02-25 15:00:35 -0600

    Agregar archivos de proyecto.

---
+++ ELECTRONICOS.AppMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ELECTRONICOS.AppMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
