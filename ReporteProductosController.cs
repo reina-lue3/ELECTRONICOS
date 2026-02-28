2a8ab2f81db46929a98054ee7c51a5477026d00d
nuriarodrigue <nuriarodrigue7700@gmail.com>
2026-02-27 22:20:53 -0600

    Agregue controlador y vista de reporte de producto
    Se puede ver los productos menos vendidos

---
+++ ReporteProductosController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ELECTRONICOS.App.MVC.Controllers
+{
+    public class ReporteProductosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
+
+
+public class ReporteProductoController : Controller
+{
+    public IActionResult Index()
+    {
+        // Usamos una lista de objetos anónimos para no depender de ninguna clase "Model"
+        var datosReporte = new List<object>
+        {
+            // Estos son ejemplos, aquí conectarías tu lógica de datos
+            new { Marca = "Toyota", Modelo = "2024", Descripcion = "Repuesto de Motor", Precio = 450.00, Stock = 10 },
+            new { Marca = "Nissan", Modelo = "2023", Descripcion = "Filtro de Aire", Precio = 25.50, Stock = 150 }
+        };
+
+        ViewBag.ProductosSinVentas = datosReporte;
+
+        return View();
+    }
+}
